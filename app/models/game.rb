3af173ba3f54c511586b681cb6394d290279d45b
Klarissa Smith <ksmith_email@yahoo.com>
2020-07-16 16:41:38 -0700

    adding validations, relationships and uncommenting cors
    Having finished the database but adding in some other things so I can focus on the database tomorrow.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+    has_many: users, through: reviews
+    has_many: user_game
 end
--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,3 @@
 class Studio < ApplicationRecord
-  belongs_to :game
+  has_many: games
 end
